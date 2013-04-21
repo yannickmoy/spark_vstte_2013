c56566678543923e5f468a6c27fcd76b2fcc745a
David MENTRE <dmentre@linux-france.org>
2013-04-21 15:03:24 +0200

    Use Unsigned_8 instead of own defined 0..255 for Octet type

--- sub3_receive_query/implementation/src/types.ads
+++ sub3_receive_query/implementation/src/types.ads
@@ -1,5 +1,7 @@
+with Interfaces; use Interfaces;
+
 package Types is
-   type Octet is range 0 .. 255; -- FIXME: how to ensure REQ-3?
+   subtype Octet is Unsigned_8; -- FIXME: how to ensure REQ-3?
 
    type Network_DNS_Query_Range is range 0..512; -- REQ-1.1
    type Network_DNS_Query is array (Network_DNS_Query_Range) of Octet; -- REQ-1
